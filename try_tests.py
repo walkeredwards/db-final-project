f105c1236ce4e36a5560927a9ea618378755d71b
Hector Ramirez <romulo-132@hotmail.com>
2023-12-02 01:44:52 -0700

    made advancements

--- try_tests.py
+++ try_tests.py
@@ -1,37 +1,28 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from pymongo.errors import OperationFailure
-import pymongo
-path_to_certificate = '/home/hectorramirez/gitHub/X509-cert-1147331512641107939.pem'
-uri = "mongodb+srv://cluster0.j1hw0tj.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
-client = MongoClient(uri,
-                     tls=True,
-                     tlsCertificateKeyFile=path_to_certificate,
-                     server_api=ServerApi('1'))
+from datetime import datetime
 
-"""
-Create database called myDatabase
-    db = client.myDatabase
-create a collection named 'recipes'
-collection is equivalent to table
-    my_collection = db.recipes
-"""
-# connects to database
-db = client['testDB']
-# selects a table == collection
-collection = db['testCol']
-# creates variable doc count and assignes it a value returned by the
-# fuction count documents
-doc_count = collection.count_documents({})
-# prints
-print(doc_count)  # Should print 0 as the testDB doesn't exist
+path_to_certificate = '/home/hectorramirez/gitHub/X509-\
+cert-1147331512641107939.pem'
+uri = 'mongodb+srv://cluster0.j1hw0tj.mongodb.net/?authSource\
+=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=\
+majority'
+client = MongoClient(
+    uri,
+    tls=True,
+    tlsCertificateKeyFile=path_to_certificate,
+    server_api=ServerApi("1"))
 
-# let's connect to sample_mflix database
-# this database is generated when you load sample data
-db = client['sample_mflix']
+# let's connect to sample_supplies database
+db = client["sample_supplies"]
 # selects table movies
-collection = db['movies']
+collection = db["inventory"]
 doc_count = collection.count_documents({})
 print(doc_count)
 
-#Beautify output using pip rich
+# Print the updated document
+updated_document = collection.find_one({})
+print(updated_document)
+
+# Beautify output using pip rich
