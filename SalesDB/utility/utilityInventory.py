62d6830aba43d725d163dacb4f2a9a4489fb9a00
Hector Ramirez <romulo-132@hotmail.com>
2023-12-04 23:00:04 -0700

    working utility lacks Del and update

--- SalesDB/utility/utilityInventory.py
+++ SalesDB/utility/utilityInventory.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.errors import OperationFailure
 from datetime import datetime
 # from pymongo.errors import OperationFailure
 # from datetime import datetime
@@ -17,7 +18,8 @@ client = MongoClient(
     tlsCertificateKeyFile=path_to_certificate,
     server_api=ServerApi("1"))
 
-class inveventoryItem:
+
+class InventoryItem:
     def __init__(self, name, tags, pricePaid, quantity):
         self.name = name
         self.tags = tags
@@ -27,68 +29,91 @@ class inveventoryItem:
 
 def newShipment(collection):
     current_datetime = datetime.now()
-    arrivalTime = current_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
-    location = input("What warehouse location this shipment arrived?")
-    suplier = input("From wich suplier?")
-    howMany = int(input("How many items arrived in this shipment?"))
-    print(howMany)
-    howMany = howMany-1
-    print(howMany)
-    itemClass = []
-    for i in range(howMany):
-        print(i)
-        if i == 0:
-            name = input("Whats the name of the item?")
-            quantity = int(input("How many arrived?"))
-            pridePaid = float(input("Whats the price paid for this shipment?"))
-            tagsBool = input("Would you like to add tags?(Y/n)")
-            if tagsBool == "Y" or tagsBool == "y":
-                tagsBool = 1
-            else:
-                tagsBool = 0
-            if tagsBool:
-                tags = []
-                x = int(input("How many would you like to add:"))
-                for i in range(x):
-                    if i == 0:
-                        tags.append(str(input("")))
-                    else:
-                        tags.append(str(input(",")))
-            itemClass.append(inveventoryItem(name, tags, pridePaid, quantity))
-            
-            document = {
-            "arrivalDate": arrivalTime,
-            "Items": itemClass,
-            "storageLocation": location,
-            "supplier": suplier
-            }
-            collection.insert_one(document)
-            print("Document created successfully.")
-            
-        if i > 1:
-            name = input("Whats the name of the item?")
-            quantity = int(input("How many arrived?"))
-            pridePaid = float(input("Whats the price paid for this shipment?"))
-            tagsBool = input("Would you like to add tags?(Y/n)")
-            if tagsBool == "Y" or tagsBool == "y":
-                tagsBool = 1
-            else:
-                tagsBool = 0
-            if tagsBool:
-                tags = []
-                x = int(input("How many would you like to add:"))
-                for i in range(x):
-                    if i == 0:
-                        tags.append(str(input("")))
-                    else:
-                        tags.append(str(input(",")))
-            itemClass.append(newShipment(name, tags, pridePaid, quantity))
-            print(itemClass[i])
-            try:
-                collection.update_one({"arrivalDate": arrivalTime}, {"$push": {"Items": itemClass[i]}})
-                print("Item pushed sucssecfully.")
-            except:
-                print("Item push failed")
-            
+    arrival_time = current_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+    location = input("What warehouse location this shipment arrived? ")
+    supplier = input("From wich suplier? ")
+    item_count = int(input("How many items arrived in this shipment? "))
 
-   
+    items = []
+    for _ in range(item_count):
+        name = input("What is the name of the item? ")
+        quantity = int(input("How many units arrived? "))
+        price_paid = float(input("What is the price paid for this item? "))
+        add_tags = input("Would you like to add tags? (Y/n) ").lower() == 'y'
+
+        tags = []
+        if add_tags:
+            tag_count = int(input("How many tags would you like to add? "))
+            for i in range(tag_count):
+                tags.append(str(input(f"Enter tag #{i + 1}: ")))
+        if _ == 0:
+            items.append(InventoryItem(name, tags, price_paid, quantity))
+            print(items[_].tags)
+            shipment_document = {
+                "arrivalDate": arrival_time,
+                "Items": [{"name": items[0].name, "tags": items[0].tags, "pricePaid": items[0].pricePaid, "quantity": items[0].quantity}],
+                "storageLocation": location,
+                "supplier": supplier
+            }
+
+            try:
+                collection.insert_one(shipment_document)
+                print("Document created successfully.")
+            except OperationFailure as ex:
+                raise ex
+        if _ > 0:
+            items.append(InventoryItem(name, tags, price_paid, quantity))
+            item_document = {
+                "arrivalDate": arrival_time,
+                "Items": [{"name": items[_].name, "tags": items[_].tags, "pricePaid": items[_].pricePaid, "quantity": items[_].quantity}],
+                "storageLocation": location,
+                "supplier": supplier
+            }
+            try:
+                collection.update_one(
+                    {"arrivalDate": arrival_time},
+                    {"$push": {
+                        "Items": {
+                            "name": items[_].name,
+                            "tags": items[_].tags,
+                            "pricePaid": items[_].pricePaid,
+                            "quantity": items[_].quantity
+                        }
+                    }}
+                )
+                print("Document appended successfully.")
+            except OperationFailure as ex:
+                raise ex
+
+def findShipment(collection, supplier):#maybe select a diferent way of looking
+    try:
+        result = collection.find_one({"supplier": supplier})
+        if result:
+            print("Shipment found:")
+            print(result)
+        else:
+            print("Shipment not found.")
+    except OperationFailure as ex:
+        raise ex
+
+def updateShipment(collection, arrival_time):
+    try:
+        # Assuming you want to update the entire shipment
+        new_location = input("Enter the new warehouse location: ")
+        new_supplier = input("Enter the new supplier: ")
+
+        collection.update_one(
+            {"arrivalDate": arrival_time},
+            {"$set": {"storageLocation": new_location, "supplier": new_supplier}}
+        )
+
+        print("Shipment updated successfully.")
+    except OperationFailure as ex:
+        raise ex
+
+def deleteShipment(collection, arrival_time):
+    try:
+        collection.delete_one({"arrivalDate": arrival_time})
+        print("Shipment deleted successfully.")
+    except OperationFailure as ex:
+        raise ex
