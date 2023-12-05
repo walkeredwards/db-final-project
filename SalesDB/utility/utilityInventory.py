ff181bbded188041ccb031a4d97ca6ad4cea1100
Hector Ramirez <romulo-132@hotmail.com>
2023-12-04 17:25:07 -0700

    updated creating a collection

--- SalesDB/utility/utilityInventory.py
+++ SalesDB/utility/utilityInventory.py
@@ -6,11 +6,11 @@ from datetime import datetime
 
 path_to_certificate = '/home/hectorramirez/gitHub/X509-\
 cert-1147331512641107939.pem'
-#path_to_certificate =''
+# path_to_certificate =''
 uri = 'mongodb+srv://cluster0.j1hw0tj.mongodb.net/?authSource\
 =%24external&authMechanism=MONGODB-X509&retryWrites=true&w=\
 majority'
-#uri = ''
+
 client = MongoClient(
     uri,
     tls=True,
@@ -36,14 +36,40 @@ def newShipment(collection):
             name = input("Whats the name of the item?")
             quantity = int(input("How many arrived?"))
             pridePaid = float(input("Whats the price paid for this shipment?"))
-            itemsClass = inveventoryItem
+            tagsBool = input("Would you like to add tags?(Y/n)")
+            if tagsBool == "Y" or tagsBool == "y":
+                tagsBool = 1
+            else:
+                tagsBool = 0
+            if tagsBool:
+                tags = []
+                for i in int(input("How many would you like to add")):
+                    tags.append(str(input(":")))
+
+            itemsClass = inveventoryItem(name, tags, pridePaid, quantity)
         if i > 1:
-            
-    document = {
-        'arrivalDate': arrivalTime,
-        'Items': [],
-        'storageLocation': location,
-        'supplier': suplier
-    }
-    collection.insert_one(document)
-    print("Document created successfully.")
+            name = input("Whats the name of the item?")
+            quantity = int(input("How many arrived?"))
+            pridePaid = float(input("Whats the price paid for this shipment?"))
+            tagsBool = input("Would you like to add tags?(Y/n)")
+            if tagsBool == "Y" or tagsBool == "y":
+                tagsBool = 1
+            else:
+                tagsBool = 0
+            if tagsBool:
+                tags = []
+                for i in int(input("How many would you like to add")):
+                    tags.append(str(input(":")))
+
+            itemsClass = inveventoryItem(name, tags, pridePaid, quantity)
+    try:        
+        document = {
+            'arrivalDate': arrivalTime,
+            'Items': [itemsClass],
+            'storageLocation': location,
+            'supplier': suplier
+        }
+        collection.insert_one(document)
+        print("Document created successfully.")
+    except:
+        print("Document creation failed")
